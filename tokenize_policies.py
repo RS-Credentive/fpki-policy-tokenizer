06f14e745cee2b0abff61a0af5da76eba6ab90c3
Robert Sherwood <88669393+rsherwood-gsa@users.noreply.github.com>
2023-12-22 11:33:58 +0000

    fixed and refactored

--- tokenize_policies.py
+++ tokenize_policies.py
@@ -6,11 +6,14 @@ import re
 nlp = stanza.Pipeline('en', download_method=stanza.DownloadMethod.REUSE_RESOURCES)
 
 def get_sentences(input: str) -> list[str]:
-    processed_text: stanza.Document = nlp(str)
-    return [sentence.text for sentence in processed_text.sentences]
+    sentences: list[str] = []
+    processed_text: stanza.Document = nlp(input)
+    for sentence in processed_text.sentences:
+        sentences.append(sentence.text)
+    return sentences
 
 
-for markdown_doc in glob.glob("converted_markdown/*.md"):
+for markdown_doc in glob.glob("converted_markdown/minimal_test.md"):
     # Calculate the target filename from the source filename
     target_doc = pathlib.Path.joinpath(pathlib.Path.cwd(), pathlib.Path("tokenized_markdown"), pathlib.Path(markdown_doc).name)
 
@@ -43,14 +46,14 @@ for markdown_doc in glob.glob("converted_markdown/*.md"):
                 if matched_elements["target"] != "":
                     first_line += f"({matched_elements['target']})"
                 if matched_elements["text"] != "":
-                    sentences = get_sentences(matched_elements["text"])
+                    sentences = get_sentences(input=matched_elements["text"])
                     first_line += sentences[0]
                     processed_lines.append(first_line)
                     # Add the rest of the sentences immediately after
                     processed_lines.extend([sentence for sentence in sentences])     
             else:
                 # This line starts with '[' but is not a link - just copy it like a regular line
-                processed_lines.extend(get_sentences(line))
+                processed_lines.extend(get_sentences(input=line))
                 
         # if the line looks like a table
         elif line[0] == "|":
@@ -59,7 +62,7 @@ for markdown_doc in glob.glob("converted_markdown/*.md"):
             # for each column, split into sentences - reassemble with a sentance per line
             # takes advantage of the fact that MD counts a single \n as equivalent to a space.
             for column in columns:
-                sentences = get_sentences(column)
+                sentences = get_sentences(input=column)
                 # split strips the separater, so we'll need to add it back at the beginning of the first sentence
                 if len(sentences) > 0:
                     sentences[0] = "|" + sentences[0]
@@ -69,8 +72,7 @@ for markdown_doc in glob.glob("converted_markdown/*.md"):
 
         # If we get here, we're probably dealing with a regular line of text 
         else:
-            sentences = get_sentences(line)
-            processed_lines.extend(line)
+            processed_lines.extend(get_sentences(input=line))
 
     # Write out the tokenized file
-    target_doc.write_text()
+    target_doc.write_text("\n".join(processed_lines), encoding="utf-8")
